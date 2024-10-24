a2e515a15d1163f91164877fa87a77ecfeb92dd6
Naveen <105493329+Naveennaveen03@users.noreply.github.com>
2024-10-24 11:34:50 +0530

    Add files via upload

---
+++ multple.js
@@ -0,0 +1,41 @@
+let a = [1, 2, 3, 4];
+let c = [];
+for (i = 0; i < 4; i++) {
+  b = a[i] * 2;
+
+  console.log(b);
+}
+
+let x = [1, 2, 3, 4];
+sum = 0;
+for (i = 0; i < 4; i++) {
+  y = sum + x[i];
+  sum = y;
+}
+console.log("sum of numbers", y);
+
+
+
+
+
+const n = [1, 2, 3];
+{
+const m = n;
+m.splice(3, 0, 4, 5);
+
+console.log("new array", m);
+}
+console.log(n)
+
+
+
+
+
+
+
+const z = [1, 2, 3];
+const l=()=>{
+    
+}
+
+
