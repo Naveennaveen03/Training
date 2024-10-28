650621cfef20008d9eadd75e699fdec7d93ee462
naveen <naveennaveen56268@gmail.com>
2024-10-28 19:20:46 +0530

    react project

--- javascript/multple.js
+++ javascript/multple.js
@@ -14,30 +14,47 @@ for (i = 0; i < 4; i++) {
 }
 console.log("sum of numbers", y);
 
+// const n = [1, 2, 3];
+// {
+//   const m = n;
+//   m.splice(3, 0, 4, 5);
 
-
-
-
-const n = [1, 2, 3];
-{
-const m = n;
-m.splice(3, 0, 4, 5);
-
-console.log("new array", m);
-}
-console.log(n)
-
+//   console.log("new array", m);
+// }
+// console.log(n);
 
 const z = [1, 2, 3];
-const l=()=>{
-    
+let s = [...z];
+s.splice(3, 0, 4, 5);
+console.log("new array",s);
+console.log("old array",z);
+
+// Filter an array of scores and return those above the average score. [70, 85, 90, 60, 88, 95, 72]
+// 5)
+
+score = [70, 85, 90, 60, 88, 95, 72];
+sum1 = 0;
+for (i = 0; i < 7; i++) {
+  sum1 = sum1 + score[i];
 }
+console.log("sum",sum1);
+average = sum1 / 7;
 
-let scores = [70, 85, 90, 60, 88, 95, 72]
-let total = scores.reduce((acc, curr) => acc + curr, 0) 
+console.log("avaerage",average);
 
-let avg = scores.filter(score => score > total);
+let p = score.filter((val) => {
+  return val > average;
+});
+console.log("Filted Array",p);
 
-console.log(avg);
 
+// pattern program
 
+for (let i = 1; i <= 5; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += j + " ";
+  }
+
+  console.log(row);
+}
